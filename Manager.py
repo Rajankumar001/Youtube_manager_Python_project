85597d19a5f4f814455f618c253389d69881566e
Rajankumar001 <chaudharyrajan387@gmail.com>
2024-07-26 05:58:12 +0530

    project creted

---
+++ Manager.py
@@ -0,0 +1,10 @@
+
+
+
+while True:
+    print("\n Youtube Manager|choose an option")
+    print("1 list all youtube videos")
+    print("2 add new youtube video")
+    print("3 update youtube video")
+    print("4 Delete youtube video")
+    print("5 Exit the app");
